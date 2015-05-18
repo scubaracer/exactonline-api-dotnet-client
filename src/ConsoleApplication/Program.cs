d4c12a1c19c8667e33e8aeb68b0ae063212299c3
shtrip <shtrip@zoho.com>
2015-05-18 12:11:44 +0200

    Console application
    This is a simple application that shows how to get started using the SDK
    client.

---
+++ src/ConsoleApplication/Program.cs
@@ -0,0 +1,32 @@
+﻿using ExactOnline.Client.Models;
+using ExactOnline.Client.Sdk.Controllers;
+using System;
+using System.Diagnostics;
+using System.Linq;
+
+namespace ConsoleApplication
+{
+	class Program
+	{
+		[STAThread]
+		static void Main(string[] args)
+		{
+			// These are the authorisation properties of your app.
+			// You can find the values in the App Center when you are maintaining the app.
+			const string clientId = "00000000-0000-0000-0000-000000000000";
+			const string clientSecret = "...";
+
+			// This can be any url as long as it is identical to the callback url you specified for your app in the App Center.
+			var callbackUrl = new Uri("http://www.exact.com"); 
+
+			var connector = new Connector(clientId, clientSecret, callbackUrl);
+			var client = new ExactOnlineClient(connector.EndPoint, connector.GetAccessToken);
+
+			// Get the Code and Name of a random account in the administration
+			var fields = new[] { "Code", "Name" };
+			var account = client.For<Account>().Top(1).Select(fields).Get().FirstOrDefault();
+
+			Debug.WriteLine("Account {0} - {1}", account.Code.TrimStart(), account.Name);
+		}
+	}
+}
