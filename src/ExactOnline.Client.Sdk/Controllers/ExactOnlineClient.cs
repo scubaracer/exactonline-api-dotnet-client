c550dbc71da98f3f1d844144d46c897837a63455
Mansour Ghaoui <m.ghaoui@interpulse.nl>
2017-06-02 12:21:22 +0200

    Whitespace

--- src/ExactOnline.Client.Sdk/Controllers/ExactOnlineClient.cs
+++ src/ExactOnline.Client.Sdk/Controllers/ExactOnlineClient.cs
@@ -52,49 +52,49 @@ namespace ExactOnline.Client.Sdk.Controllers
 		{
 		}
 
-        #endregion
+		#endregion
 
-        #region Public methods
+		#region Public methods
 
-        /// <summary>
-        /// Returns the current user data
-        /// </summary>
-        /// <returns>Me entity</returns>
-        public Me CurrentMe()
-        {
-            var conn = new ApiConnection(_apiConnector, _exactOnlineApiUrl + "current/Me");
-            string response = conn.Get("$select=CurrentDivision");
-            response = ApiResponseCleaner.GetJsonArray(response);
-            var converter = new EntityConverter();
+		/// <summary>
+		/// Returns the current user data
+		/// </summary>
+		/// <returns>Me entity</returns>
+		public Me CurrentMe()
+		{
+			var conn = new ApiConnection(_apiConnector, _exactOnlineApiUrl + "current/Me");
+			string response = conn.Get("$select=CurrentDivision");
+			response = ApiResponseCleaner.GetJsonArray(response);
+			var converter = new EntityConverter();
 			var currentMe = converter.ConvertJsonArrayToObjectList<Me>(response);
 			return currentMe.FirstOrDefault();
-        }
+		}
 
-        /// <summary>
-        /// return the division number of the current user
-        /// </summary>
-        /// <returns>Division number</returns>
-        public int GetDivision()
-        {
-            if (_division > 0)
-            {
-                return _division;
-            }
+		/// <summary>
+		/// return the division number of the current user
+		/// </summary>
+		/// <returns>Division number</returns>
+		public int GetDivision()
+		{
+			if (_division > 0)
+			{
+				return _division;
+			}
 
 			var currentMe = CurrentMe();
-            if (currentMe != null)
-            {
-                _division = currentMe.CurrentDivision;
-                return _division;
-            }
+			if (currentMe != null)
+			{
+				_division = currentMe.CurrentDivision;
+				return _division;
+			}
 
-            throw new Exception("Cannot get division. Please specify division explicitly via the constructor.");
-        }
+			throw new Exception("Cannot get division. Please specify division explicitly via the constructor.");
+		}
 
-        /// <summary>
-        /// Returns instance of ExactOnlineQuery that can be used to manipulate data in Exact Online
-        /// </summary>
-        public ExactOnlineQuery<T> For<T>() where T : class
+		/// <summary>
+		/// Returns instance of ExactOnlineQuery that can be used to manipulate data in Exact Online
+		/// </summary>
+		public ExactOnlineQuery<T> For<T>() where T : class
 		{
 			var controller = _controllers.GetController<T>();
 			return new ExactOnlineQuery<T>(controller);
