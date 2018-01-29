45a851742e15f2b1163619740454a06ce5d6c7a4
tensingusa <tensingusa@gmail.com>
2018-01-28 17:23:27 -0800

    Change 'WebhookSubscriptions' to 'WebhookSubscription'

--- src/ExactOnline.Client.Models/Services.vb
+++ src/ExactOnline.Client.Models/Services.vb
@@ -220,8 +220,8 @@ Public Class [Services]
 		Services.Add(GetType(Users.UserRolesPerDivision).FullName, "Users/UserRolesPerDivision")
 		Services.Add(GetType(VAT.VATCode).FullName, "VAT/VATCodes")
         Services.Add(GetType(VAT.VatPercentage).FullName, "VAT/VatPercentages")
-        Services.Add(GetType(Webhooks.WebhookSubscriptions).FullName, "Webhooks/WebhookSubscriptions")
-		Services.Add(GetType(Workflow.RequestAttachment).FullName, "Workflow/RequestAttachments")
+        Services.Add(GetType(Webhooks.WebhookSubscription).FullName, "Webhooks/WebhookSubscriptions")
+        Services.Add(GetType(Workflow.RequestAttachment).FullName, "Workflow/RequestAttachments")
 
 	End Sub
 End Class
--- src/ExactOnline.Client.Models/Webhooks.vb
+++ src/ExactOnline.Client.Models/Webhooks.vb
@@ -2,7 +2,7 @@ Namespace Webhooks
 
     <SupportedActionsSDK(True, True, False, True)>
     <DataServiceKey("ID")>
-    Public Class [WebhookSubscriptions]
+    Public Class [WebhookSubscription]
         Public Property [ID] As Guid
         Public Property [CallbackURL] As String
         Public Property [Topic] As String
